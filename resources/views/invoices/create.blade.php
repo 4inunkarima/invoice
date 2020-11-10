b0cfe923f0f8e5e40c150f27b64830ef9c3d6b73
ainunkarima <aynunkarima@gmail.com>
2020-11-10 11:01:19 +0700

    hehe

--- resources/views/invoices/create.blade.php
+++ resources/views/invoices/create.blade.php
@@ -23,7 +23,7 @@
                                         <label for="staticEmail2" class="sr-only">Email</label>
                                         <input type="text" readonly class="form-control-plaintext" font-size="6px" id="staticEmail2" value="office@email.com">
                                     </div>
-                                    <button type="submit" class="btn btn-primary mb-2">Save Draft</button>
+                                    <button type="submit" class="btn btn-primary mb-2">Another Email</button>
                                 </form>
                                 <div class="media">
                                     <div class="media-body">
@@ -67,7 +67,7 @@
                                     <div class="row">
                                         <div class="col-md-6">
                                         <label for="Address">From</label>
-                                        <input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="Kantor LALALA">  
+                                        <input type="text" class="form-control-plaintext" id="staticEmail2" value="Kantor LALALA">  
                                         <p>Jl. Petemon IV No.32-A, RT 014/RW 008<br/>Kel. Petemon, Kec. Sawahan, Kota Surabaya<br/>Jawa Timur, 60252<br/>0839-0868-2951</p>
                                         <br/>
                                         <label for="Address">To</label>
@@ -115,29 +115,56 @@
                                     </div>
                                 </form>
                                 <hr /> 
-                                <div class="col-md-6 float-right">                                  
-                                    <label for="Address">Sub Total</label>
-                                    <hr size="2px" width="100%" />
-                                    <h4>Total USD</h4>
-                                    <div class="input-group border border-secondary">
-                                            <div class="input-group-prepend">
-                                            <div class="input-group-text">Balance</div>
+                                <div class="totals col-md-6 float-right">
+                                    <div class="totals_container">
+										<ul class="statement_amount_container">
+                                            <li class="statement_amount">
+                                                <div class="col">Sub Total</div>
+                                                <input class="col no_input" type="text" disabled="" data-format-number="precision" value="0.00" data-calculation="subtotal">
+                                            </li>
+                                        </ul>                                                         
+                                        <div id="tax_discount_shipping_wrapper">
+                                        </div>
+                                        <div id="item_tax_discount_shipping_wrapper">
+                                        </div>
+                                        <ul class="statement_amount_container">
+                                        <li class="statement_amount no_border">
+                                            <div class="col"><strong>Total (<span data-currency="code">USD</span>)</strong></div>
+                                            <strong><input class="col no_input bold" type="text" disabled="" data-format-number="precision" data-calculation="total" value="0.00"></strong>
+                                        </li>
+                                        </ul>
+                                        <hr />
+                                        <ul class="statement_amount_container total_due draft">
+                                        <li class="statement_amount">
+                                            <div class="input-group border border-secondary">
+                                                    <div class="input-group-prepend">
+                                                    <div class="input-group-text">Balance</div>
+                                                    </div>
+                                                    <input type="text" class="form-control" id="inlineFormInputGroup" value="USD">
                                             </div>
-                                            <input type="text" class="form-control" id="inlineFormInputGroup" value="USD">
+                                        </li>
+                                        </ul>
                                     </div>
                                 </div>
                                 <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <hr />
-                                        <div class="col-md-12">
-                                        <label for="Address">Invoice Note</label>
-                                        <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Add note.." rows="4"></textarea> 
-                                </div>
+                                <form>
+                                    <div class="row">
+                                        <div class="col-md-12"> 
+                                            <div class="form-group"> 
+                                            <label for="Address">Invoice Note</label>
+                                            <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Add note.." rows="4"></textarea> 
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="checkbox" id="disabledFieldsetCheck" disabled>
+                                                    <label class="form-check-label" for="disabledFieldsetCheck">
+                                                    Data filled correctly
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
                                 <br/>
                                 <br/>
                                 <a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Send Invoice</a>
