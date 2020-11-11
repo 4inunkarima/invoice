55e4ff0c825594de01e0fa872d4214363f7cc5d0
ainunkarima <aynunkarima@gmail.com>
2020-11-11 11:01:18 +0700

    fields
    fields

--- resources/views/customers/fields.blade.php
+++ resources/views/customers/fields.blade.php
@@ -1,47 +1,151 @@
-<!-- Nama Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('nama', 'Nama:') !!}
-    {!! Form::text('nama', null, ['class' => 'form-control']) !!}
-</div>
-
-<!-- Email Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('email', 'Email:') !!}
-    {!! Form::text('email', null, ['class' => 'form-control']) !!}
-</div>
-
-<!-- Telepon Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('telepon', 'Telepon:') !!}
-    {!! Form::number('telepon', null, ['class' => 'form-control']) !!}
-</div>
-
-<!-- Alamat Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('alamat', 'Alamat:') !!}
-    {!! Form::text('alamat', null, ['class' => 'form-control']) !!}
-</div>
-
-<!-- Gender Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('gender', 'Gender:') !!}
-    {!! Form::text('gender', null, ['class' => 'form-control']) !!}
-</div>
-
-<!-- Username Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('username', 'Username:') !!}
-    {!! Form::text('username', null, ['class' => 'form-control']) !!}
-</div>
-
-<!-- Password Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('password', 'Password:') !!}
-    {!! Form::text('password', null, ['class' => 'form-control']) !!}
-</div>
-
-<!-- Submit Field -->
-<div class="form-group col-sm-12">
-    {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-    <a href="{{ route('customers.index') }}" class="btn btn-secondary">Cancel</a>
-</div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <i class="fa fa-plus-square-o fa-lg"></i>
+                                <strong>Basic Information</strong>
+                            </div>
+                            <div class="card-body">
+                                <div class="media">
+                                    <img src="views/customers/prof.png" class="mr-3" alt="prof">
+                                    <div class="media-body">
+                                    <select id="inputState" class="form-control col-md-4">
+                                            <option selected>Choose...</option>
+                                            <option>Individu</option>
+                                            <option>Organisation</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <hr />
+                                <form>
+                                    <div class="row">
+                                        <div class="col-md-3">
+                                        <label for="Address">Nama</label>
+                                        <input type="text" class="form-control" placeholder="Nama">
+                                        <br/>
+                                        <label for="Address">Email</label>
+                                        <input type="text" class="form-control" placeholder="Email">
+                                        </div>
+                                        <div class="col-md-3">
+                                        <label for="Address">Nama Organisasi</label>
+                                        <input type="text" class="form-control" placeholder="Nama Organisasi">
+                                        <br/>
+                                        <label for="Address">Username</label>
+                                        <input type="text" class="form-control" placeholder="Username">
+                                        </div>
+                                        <div class="col">
+                                        <label for="Address">Currency</label>
+                                        <select id="inputState" class="form-control">
+                                            <option selected>Choose..</option>
+                                            <option>IDR</option>
+                                            <option>USD</option>
+                                        </select>
+                                        <br/>
+                                        <label for="Address">Language</label>
+                                        <select id="inputState" class="form-control">
+                                            <option selected>Choose..</option>
+                                            <option>English</option>
+                                            <option>Indonesia</option>
+                                        </select> 
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <i class="fa fa-plus-square-o fa-lg"></i>
+                                <strong>Address</strong>
+                            </div>
+                            <div class="card-body">
+                                <form>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                        <label for="Address">Alamat</label>
+                                        <input type="text" class="form-control" placeholder="Alamat">
+                                        <br/>
+                                        <label for="Address">Alamat Tambahan</label>
+                                        <input type="text" class="form-control" placeholder="Alamat">
+                                        </div>
+                                        <div class="col-md-3">
+                                        <label for="Address">Postal Code</label>
+                                        <input type="text" class="form-control" placeholder="Postal Code">
+                                        <br/>
+                                        <label for="Address">City</label>
+                                        <input type="text" class="form-control" placeholder="City">
+                                        </div>
+                                        <div class="col-md-3">
+                                        <label for="Address">State</label>
+                                        <input type="text" class="form-control" placeholder="State">
+                                        <br/>
+                                        <label for="Address">Country</label>
+                                        <select id="inputState" class="form-control">
+                                            <option selected>Choose..</option>
+                                            <option>Indonesia</option>
+                                            <option>Brunei Darussalam</option>
+                                            <option>Kamboja</option>
+                                            <option>Laos</option>
+                                            <option>Myanmar</option>
+                                            <option>Singapura</option>
+                                            <option>Filipina</option>
+                                        </select> 
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <i class="fa fa-plus-square-o fa-lg"></i>
+                                <strong>Aditional Informations</strong>
+                            </div>
+                            <div class="card-body">
+                                <form>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <label for="Address">Telepon</label>
+                                            <input type="text" class="form-control" placeholder="Phone Number">
+                                            <br/>
+                                            <label for="Address">Gender</label>
+                                            <select id="inputState" class="form-control">
+                                                <option selected>Choose...</option>
+                                                <option>Male</option>
+                                                <option>Female</option>
+                                            </select>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <label for="Address">Fax Number</label>
+                                            <input type="text" class="form-control" placeholder="Fax Number">
+                                            <br/>
+                                            <label for="Address">Note</label>
+                                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="4"></textarea>
+                                        </div>
+                                    </div>
+                                </form>
+                                <hr />
+                                <form>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <label for="Address">Password</label>
+                                            <input type="text" class="form-control" placeholder="Password">
+                                        </div>
+                                        <div class="col-md-6">
+                                            <label for="Address">Confirm Password</label>
+                                            <input type="text" class="form-control" placeholder="Password">
+                                        </div>
+                                    </div>
+                                </form>
+                                <br/>
+                                <a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Save Data</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
