cd92f80c572fdca1bc8c2a0d6488535896316613
ainunkarima <aynunkarima@gmail.com>
2020-11-13 12:53:44 +0700

    baru

--- resources/views/produks/index.blade.php
+++ resources/views/produks/index.blade.php
@@ -16,10 +16,42 @@
                              <a class="pull-right" href="{{ route('produks.create') }}"><i class="fa fa-plus-square fa-lg"></i></a>
                          </div>
                          <div class="card-body">
-                             @include('produks.table')
-                              <div class="pull-right mr-3">
-                                     
-                              </div>
+                            <table class="table table-striped" id="produks-table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">No.</th>
+                                        <th scope="col">Nama Produk</th>
+                                        <th scope="col">Jenis Produk</th>
+                                        <th scope="col">Stok</th>
+                                        <th scope="col">Deskripsi Produk</th>
+                                        <th scope="col">Harga Produk</th>
+                                        <th scope="col">Waktu</th>
+                                        <th colspan="3">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($produks as $produk)
+                                    <tr>
+                                        <th scope="row">{{ $loop->iteration }}</th>
+                                        <td>{{ $produk->nama_produk }}</td>
+                                        <td>{{ $produk->jenis_produk }}</td>
+                                        <td>{{ $produk->stok }}</td>
+                                        <td>{{ $produk->deskripsi_produk }}</td>
+                                        <td>{{ $produk->harga_produk }}</td>
+                                        <td>{{ $produk->created_at }}</td>
+                                            <td>
+                                            {!! Form::open(['route' => ['produks.destroy', $produk->id], 'method' => 'delete']) !!}
+                                            <div class='btn-group'>
+                                                <a href="{{ route('produks.show', [$produk->id]) }}" class='btn btn-ghost-success'><i class="fa fa-eye"></i></a>
+                                                <a href="{{ route('produks.edit', [$produk->id]) }}" class='btn btn-ghost-info'><i class="fa fa-edit"></i></a>
+                                                {!! Form::button('<i class="fa fa-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-ghost-danger', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                                            </div>
+                                            {!! Form::close() !!}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
                          </div>
                      </div>
                   </div>
