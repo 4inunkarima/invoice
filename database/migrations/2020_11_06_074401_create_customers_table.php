31fdfef4d7a58abc1c68a653a6e9eb3790734598
alfanizidni <48055492+alfanizidni@users.noreply.github.com>
2020-11-19 15:08:37 +0700

    edit migration

--- database/migrations/2020_11_06_074401_create_customers_table.php
+++ database/migrations/2020_11_06_074401_create_customers_table.php
@@ -17,11 +17,22 @@ class CreateCustomersTable extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nama');
+            $table->string('organisasi');
+            $table->string('mata_uang');
             $table->string('email');
             $table->string('telepon');
+            $table->integer('fax_number');
+            $table->string('note');
+            $table->string('konfirmasi_password');
             $table->string('alamat');
+            $table->integer('kode_pos');
+            $table->string('state');
+            $table->string('alamat_tambahan');
+            $table->string('kota');
+            $table->string('negara');
             $table->string('gender');
             $table->string('username');
+            $table->string('bahasa');
             $table->string('password');
             $table->timestamps();
             $table->softDeletes();
