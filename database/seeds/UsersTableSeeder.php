4cc46ef9ef34f2806ce43cd24a6087173c264d86
aliffi98 <48055511+aliffi98@users.noreply.github.com>
2020-12-03 11:40:30 +0700

    User Seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,8 +13,8 @@ class UsersTableSeeder extends Seeder
     {
         DB::table('users')->insert([
             'name' => 'admin',
-            'email' => 'admin@gmail.com',
-            'password' => '$2y$10$dv2xVZD0ziRw1Qt5Y3nlVeJTQzSdw24whpuFq5dCAZwK8WmCUrdh6'
+            'email' => 'admin@administrator.com',
+            'password' =>Hash::make('admin123'),
         ]); 
     }
 }
